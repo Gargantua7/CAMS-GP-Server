374333219164e6178222df05df445baabad54b65
Gargantua <1505011271@qq.com>
2021-12-27 14:36:05 +0800

    First Initialize

---
+++ src/main/kotlin/com/gargantua7/cams/gp/server/CamsGpServerApplication.kt
@@ -0,0 +1,11 @@
+package com.gargantua7.cams.gp.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamsGpServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CamsGpServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/gargantua7/cams/gp/server/CamsGpServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gargantua7.cams.gp.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamsGpServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
