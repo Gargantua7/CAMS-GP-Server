2539a86a29a1af47dc27c24f3db7f0ef38273e74
Gargantua <1505011271@qq.com>
2022-02-23 02:59:52 +0800

    Changes made to Repair will be synchronized to Reply, and requests that do not need to be executed will no longer be executed.

--- src/main/kotlin/com/gargantua7/cams/gp/server/services/RepairService.kt
+++ src/main/kotlin/com/gargantua7/cams/gp/server/services/RepairService.kt
@@ -2,9 +2,11 @@ package com.gargantua7.cams.gp.server.services
 
 import com.gargantua7.cams.gp.server.dao.PersonDao
 import com.gargantua7.cams.gp.server.dao.RepairDao
+import com.gargantua7.cams.gp.server.dao.ReplyDao
 import com.gargantua7.cams.gp.server.exception.AuthorizedException
 import com.gargantua7.cams.gp.server.exception.NotFoundException
 import com.gargantua7.cams.gp.server.model.dto.Repair
+import com.gargantua7.cams.gp.server.model.dto.Reply
 import org.apache.shiro.SecurityUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -18,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional
 class RepairService {
 
     @Autowired
+    private lateinit var replyDao: ReplyDao
+
+    @Autowired
     private lateinit var personDao: PersonDao
 
     @Autowired
@@ -28,8 +33,17 @@ class RepairService {
     }
 
     fun assignPrincipleByUUID(uuid: String, principle: String) {
-        if (repairDao.assignPrincipleByUUID(uuid, principle) != 1)
-            throw NotFoundException("Repair[$uuid] Not Found")
+        val repair = repairDao.selectRepairByUUID(uuid)
+        if (repair.principal == principle) return
+        repairDao.assignPrincipleByUUID(uuid, principle)
+        replyDao.insertNewReply(
+            Reply(
+                repair = uuid,
+                sender = SecurityUtils.getSubject().principal as String,
+                type = 2,
+                content = principle
+            )
+        )
     }
 
     fun selectAllRepairUUIDListWithPage(page: Int): List<String> {
@@ -63,8 +77,17 @@ class RepairService {
             if (requesterId != repair.initiator && requesterId != repair.principal)
                 throw AuthorizedException.InsufficientPermissionsException()
         }
-        if (repairDao.changeStateByUUID(uuid, state) != 1)
-            throw NotFoundException("Repair[$uuid] Not Found")
+        val repair = repairDao.selectRepairByUUID(uuid)
+        if (repair.state == state) return
+        repairDao.changeStateByUUID(uuid, state)
+        replyDao.insertNewReply(
+            Reply(
+                repair = uuid,
+                sender = requesterId,
+                type = 1,
+                content = state.toString()
+            )
+        )
     }
 
     fun <P, R> securityCalled(param: P, action: (P, String) -> R): R {
